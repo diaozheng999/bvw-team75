3056036e8885583edb7ee54f0c7360a853bb62cc
Lotusly <yul4@andrew.cmu.edu>
2017-11-19 14:33:37 -0500

    frame for timer.

---
+++ Cashier/Assets/Scripts/Shared/Door.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Team75.Shared
+{
+
+
+
+	public class Door : MonoBehaviour
+	{
+		private int state=0;
+		// 0 for closed; 1 for opening; 2 for opened; 3 closing
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+
+
+}
