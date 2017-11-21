2844f21509bff8de5be892e09767e1e816dfc7d4
Lotusly <yul4@andrew.cmu.edu>
2017-11-21 15:19:53 -0500

    add avatar & items to website. remix & set prices

--- WebClient/items.js
+++ WebClient/items.js
@@ -1,9 +1,201 @@
 var remoteServer = "http://128.2.237.82:47506/"
 
 var itemDictionary = [
+
+    {
+        "itemId" : "29",
+        "price" : "399",
+        "title" : "Burger King",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/29.jpg"
+    }
+
+    ,
+
+    {
+        "itemId" : "14",
+        "price" : "220",
+        "title" : "Popcorn",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/14.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "12",
+        "price" : "220",
+        "title" : "Rubber Duck",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "15",
+        "price" : "180",
+        "title" : "Yellow Balloon",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "16",
+        "price" : "180",
+        "title" : "Blue Balloon",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "17",
+        "price" : "180",
+        "title" : "Green Balloon",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "18",
+        "price" : "180",
+        "title" : "Brown Balloon",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "19",
+        "price" : "180",
+        "title" : "Purple Balloon",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "20",
+        "price" : "180",
+        "title" : "Pink Soap",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "21",
+        "price" : "180",
+        "title" : "Gold Soap",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "22",
+        "price" : "180",
+        "title" : "Light-Blue Soap",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "23",
+        "price" : "180",
+        "title" : "Deep-Blue Soap",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "24",
+        "price" : "220",
+        "title" : "Spider",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "25",
+        "price" : "220",
+        "title" : "Blue Dog",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "26",
+        "price" : "220",
+        "title" : "Goal Dog",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "27",
+        "price" : "220",
+        "title" : "Red Dog",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+    
+
+    {
+        "itemId" : "30",
+        "price" : "150",
+        "title" : "Switch",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/30.jpg"
+    }
+
+    ,
+
+    {
+        "itemId" : "10",
+        "price" : "100",
+        "title" : "Apple",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
+    {
+        "itemId" : "11",
+        "price" : "100",
+        "title" : "Pear",
+        "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
+        "image" : "images/items/3.png"
+    }
+
+    ,
+
     {
         "itemId" : "0",
-        "price" : "50",
+        "price" : "80",
         "title" : "Moonshine",
         "description" : "Genuine vintage brew from 1400s.",
         "image" : "images/items/0.png"
@@ -11,7 +203,7 @@ var itemDictionary = [
 
     {
         "itemId" : "1",
-        "price" : "30",
+        "price" : "80",
         "title" : "Fresh Banana (1pc)",
         "description" : "Freshly flown from the Bahamas.",
         "image" : "images/items/1.png"
@@ -27,7 +219,7 @@ var itemDictionary = [
     
     {
         "itemId" : "3",
-        "price" : "40",
+        "price" : "80",
         "title" : "Freshly Baked Croissant",
         "description" : "Definitely French.",
         "image" : "images/items/3.png"
@@ -36,7 +228,7 @@ var itemDictionary = [
     
     {
         "itemId" : "4",
-        "price" : "25",
+        "price" : "80",
         "title" : "Bread",
         "description" : "Bread! Very good bread! Freshly baked.",
         "image" : "images/items/4.png"
@@ -45,7 +237,7 @@ var itemDictionary = [
     
     {
         "itemId" : "5",
-        "price" : "25",
+        "price" : "80",
         "title" : "Bread",
         "description" : "Sometimes, we need more than one type of bread.",
         "image" : "images/items/5.png"
@@ -54,7 +246,7 @@ var itemDictionary = [
     
     {
         "itemId" : "6",
-        "price" : "60",
+        "price" : "100",
         "title" : "Cracky Vinegar",
         "description" : "Sour. Very sour. Extremely sour.",
         "image" : "images/items/6.png"
@@ -64,7 +256,7 @@ var itemDictionary = [
     
     {
         "itemId" : "7",
-        "price" : "80",
+        "price" : "100",
         "title" : "Whole Milk",
         "description" : "Moo!!!!!",
         "image" : "images/items/7.png"
@@ -79,4 +271,9 @@ var itemDictionary = [
         "description" : "Tomato sauce for the uninitiated. Definitely not from Pittsburgh.",
         "image" : "images/items/3.png"
     }
+
+    
+
+    
+
 ]
